d1109393987d7793c0682d34af336ccc4db16d3e
Chinthani <chinthani96@gmail.com>
2020-10-28 12:55:00 +0530

    Refactor Library Component to display Book i.e child component dynamically

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React, { Component} from "react"
 import { render } from "react-dom"
 
+const bookList = [
+    {"title":"Inferno", "author":"Dan Brown", "pages":700},
+    {"title":"Da Vinci Code", "author":"Dan Brown", "pages":600},
+    {"title":"Book 3", "author":"Author 3", "pages":200},
+    {"title":"Harry Potter", "author":"J K Rowling", "pages":300},
+    {"title":"Famous Five", "author":"Enid Blyton", "pages":1000},
+    {"title":"Sherlock Holmes", "author":"Author Conon Doyle", "pages":30}
+]
+
 const Book = ({title,author,pages}) => {
     return(
         <section>
-            Hey there I'm a book!
             <h2>{title}</h2>
             <h5>{author}</h5>
             <p>{pages} pages</p>
@@ -12,19 +20,20 @@ const Book = ({title,author,pages}) => {
     )
 }
 
-const Library = () => {
+const Library = ({books}) => {
     return(
         <div>
-            <Book title="Inferno" author="Dan Brown" pages={700}/>
-            <Book title="Da Vinci Code" author="Dan Brown" pages={700}/>
-            <Book title="Book 3" author="Dan Brown" pages={700}/>
-            <Book title="Book 4" author="Dan Brown" pages={700}/>
-            <Book title="Book 6" author="Dan Brown" pages={700}/>
+            {books.map(
+                (book,i) => <Book
+                    key={i}
+                    title={book.title}
+                    author={book.author}
+                    pages={book.pages}/>
+            )}
         </div>
     )
 }
-
 render(
-    <Library />,
+    <Library books={bookList}/>,
     document.getElementById("root")
 )
