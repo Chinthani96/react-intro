6926e7161f395d2aaaf7d2737df0411da9d6f66d
Chinthani <chinthani96@gmail.com>
2020-10-28 12:41:10 +0530

    Add Book component and render it to the Library component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React, { Component} from "react"
 import { render } from "react-dom"
 
+const Book = ({title,author,pages}) => {
+    return(
+        <section>
+            Hey there I'm a book!
+            <h2>{title}</h2>
+            <h5>{author}</h5>
+            <p>{pages} pages</p>
+        </section>
+    )
+}
+
 const Library = () => {
     return(
-        <div>Hey this is the Library!</div>
+        <div>
+            <Book title="Inferno" author="Dan Brown" pages={700}/>
+            <Book title="Da Vinci Code" author="Dan Brown" pages={700}/>
+            <Book title="Book 3" author="Dan Brown" pages={700}/>
+            <Book title="Book 4" author="Dan Brown" pages={700}/>
+            <Book title="Book 6" author="Dan Brown" pages={700}/>
+        </div>
     )
 }
 
