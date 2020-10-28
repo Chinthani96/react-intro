78439c259aebe5d3f8dbc2669d22db4f78601cce
Chinthani <chinthani96@gmail.com>
2020-10-28 12:25:34 +0530

    (2)Start playing around with props and state

--- src/index.js
+++ src/index.js
@@ -1,48 +1,8 @@
 import React, { Component} from "react"
 import { render } from "react-dom"
 
-let skiData = {
-    total: 10,
-    powder: 5,
-    backCountry: 2,
-    goal: 100
-}
-
-const getPercent = decimal => {
-    return decimal * 100 + '%';
-}
-const calcGoalProgress = (total,goal) => {
-    return getPercent(total/goal);
-}
-
-//functional component with all the prop fields passed
-const SkiDataCounter = ({total,powder,backCountry,goal}) => {
-    return (
-        <section>
-            <div>
-                <p>Total days: {total}</p>
-            </div>
-            <div>
-                <p>Powder days: {powder}</p>
-            </div>
-            <div>
-                <p>Back Country days: {backCountry}</p>
-            </div>
-            <div>
-                <p>Goals: {goal}</p>
-            </div><div>
-                <p>Goal progress: {calcGoalProgress(total,goal)}</p>
-            </div>
-        </section>
-    )
-}
 
 render(
-    <SkiDataCounter
-        total={skiData.total}
-        powder={skiData.powder}
-        backCountry={skiData.backCountry}
-        goal={skiData.goal}
-    />,
+
     document.getElementById("root")
 )
