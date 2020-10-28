6d02069f72cbc4feb2c286c31776c47c0cd1c9ac
Chinthani <chinthani96@gmail.com>
2020-10-28 12:30:29 +0530

    Add Library component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React, { Component} from "react"
 import { render } from "react-dom"
 
+const Library = () => {
+    return(
+        <div>Hey this is the Library!</div>
+    )
+}
 
 render(
-
+    <Library />,
     document.getElementById("root")
 )
