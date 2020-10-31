82dc30f9f2de882c1fdd20bcadc72f17912738ac
Chinthani <chinthani96@gmail.com>
2020-10-31 10:32:59 +0530

    Add a form for a colorpicker component

--- src/index.js
+++ src/index.js
@@ -1,100 +1,30 @@
 import React, {Component} from "react"
 import {render} from "react-dom"
 
-const bookList = [
-    {"title": "Inferno", "author": "Dan Brown", "pages": 700},
-    {"title": "Da Vinci Code", "author": "Dan Brown", "pages": 600},
-    {"title": "Book 3", "author": "Author 3", "pages": 200},
-    {"title": "Harry Potter", "author": "J K Rowling", "pages": 300},
-    {"title": "Famous Five", "author": "Enid Blyton", "pages": 1000},
-    {"title": "Sherlock Holmes", "author": "Author Conon Doyle", "pages": 30}
-]
-
-const Hiring = () =>
-    <div>
-        <p>Library is hiring!</p>
-    </div>
-
-const NotHiring = () =>
-    <div>
-        <p>Library is not hiring!</p>
-    </div>
-
-const Book = ({title, author, pages, freeBookmark}) => {
-    return (
-        <section>
-            <h2>{title}</h2>
-            <h5>{author}</h5>
-            <p>{pages} pages</p>
-            <p>Free bookmark today : {freeBookmark ? 'Yes!' : 'No!'}</p>
-        </section>
-    )
-}
-
-class Library extends Component {
+class ColorPicker extends Component{
     state = {
-        open: true,
-        freeBookmark: false,
-        hiring: false,
-        data: [],
-        loading: false
+        value: ''
+    }
+    newColor = e =>
+     this.setState({value: e.target.value})
+
+    submit = e => {
+        console.log("New Color: "+this.state.value)
+        e.preventDefault()
     }
 
-    componentDidMount() {
-        this.setState({loading: true})
-        fetch('https://hplussport.com/api/products/order/price/sort/asc/qty/1')
-            .then(data => data.json())
-            .then(data => this.setState({data, loading: false}))
-    }
-
-    componentDidUpdate() {
-        console.log("Component updated!");
-    }
-
-    toggleOpenClosed = () => {
-        this.setState(prevState => ({
-            open: !prevState.open
-        }))
-    }
-
-    render() {
-        const {books} = this.props;
-        // console.log(this.state);
-        return (
-            <div>
-                {this.state.hiring ? <Hiring/> : <NotHiring/>}
-                {this.state.loading
-                    ? "loading..."
-                    : <div>
-                        {this.state.data.map(product => {
-                            return(
-                                <div key={product.id}>
-                                    <h4>The product of the week!</h4>
-                                    <h5>{product.name}</h5>
-                                    <img alt={product.name} src={product.image} height={100} />
-                                </div>
-                            )
-                        })}
-                    </div>
-                }
-                <h1>The Library is {this.state.open ? "Open" : "Closed"}</h1>
-                <button onClick={this.toggleOpenClosed}>Change</button>
-                {books.map(
-                    (book, i) => <Book
-                        key={i}
-                        title={book.title}
-                        author={book.author}
-                        pages={book.pages}
-                        freeBookmark={this.state.freeBookmark}
-                    />
-                )}
-            </div>
+    render(){
+        return(
+            <form onSubmit={this.submit}>
+                <label>Favorite color: </label>
+                <input type="color" onChange={this.newColor}/>
+                <button>Submit</button>
+            </form>
         )
     }
-
 }
 
 render(
-    <Library books={bookList}/>,
+    <ColorPicker />,
     document.getElementById("root")
 )
